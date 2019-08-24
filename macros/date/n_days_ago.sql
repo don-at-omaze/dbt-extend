818d1cf1c53dc2f3a775949d2ee0bb974d1e4a75
Claus Herther <claus@calogica.com>
2019-08-24 16:52:30 -0600

    Adds periods_since and updates time conversion macros

--- macros/date/n_days_ago.sql
+++ macros/date/n_days_ago.sql
@@ -1,5 +1,4 @@
 {%- macro n_days_ago(n, tz=None) -%}
-{% set tz = var("dbt_extend:time_zone") if not tz else tz %}
 {%- set n = n|int -%}
 {{ dbt_utils.dateadd('day', -1 * n, dbt_extend.get_local_date(tz)) }}
 {%- endmacro -%}
--- macros/date/this_week.sql
+++ macros/date/this_week.sql
@@ -1,4 +1,3 @@
 {%- macro this_week(tz=None) -%}
-{% set tz = var("dbt_extend:time_zone") if not tz else tz %}
 {{ dbt_utils.date_trunc('week', dbt_extend.get_local_date(tz)) }}
 {%- endmacro -%}
