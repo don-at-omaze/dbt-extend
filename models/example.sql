298ee51cb708efc25f80bd7da43dff56651a769d
Claus Herther <claus@calogica.com>
2019-10-04 13:08:54 -0700

    Updates example model

--- models/example.sql
+++ models/example.sql
@@ -5,8 +5,8 @@
 }}
 select 
     {{ dbt_extend.get_local_time() }} as currently,
-    {{ dbt_extend.today() }} as today,
+    {{ dbt_extend.get_local_date() }} as today,
     {{ dbt_extend.yesterday() }} as today,
-    {{ dbt_extend.tomorrow() }} as tomorrow,
+    {{ dbt_date.tomorrow() }} as tomorrow,
     {{ dbt_extend.convert_timezone('currently') }} as the_converted_time,
     {{ dbt_extend.convert_timezone('today') }} as the_converted_date
